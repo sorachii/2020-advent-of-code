7e2ff6221adc270d341fb759ca045d23c71196e7
sorachii <bencecube@gmail.com>
2020-12-05 00:19:47 +0100

    fourth star was not that hard after knowing how regex works

--- day2/main.py
+++ day2/main.py
@@ -20,4 +20,23 @@ def ans1():
             count += 1
     return count
 
+def ans2():
+    count = 0
+    for line in lista:
+        lo, hi, char, string = parse(line)
+        if (string[lo - 1] == char) != (string[hi - 1] == char):
+            count += 1
+    return count
+
+# just for testing I wanted to print a 'correct' line
+def ans3():
+    count = 0
+    for line in lista:
+        lo, hi, char, string = parse(line)
+        if (string[lo - 1] == char) != (string[hi - 1] == char):
+            return line
+
+
 print(ans1())
+print(ans2())
+print(ans3())
