8eccde7d72ea61d3a3916cd119c0b36773301e11
sorachii <bencecube@gmail.com>
2020-12-05 21:46:17 +0100

    some formatting

--- day3/main.py
+++ day3/main.py
@@ -2,17 +2,17 @@ def ans1():
     count = 0
     r = 0
     c = 0
-    while r+1 < len(G):
+    while r + 1 < len(G):
         r += 1
         c += 3
-        if G[r][c%len(G[r])] == '#':
+        if G[r][c % len(G[r])] == "#":
             count += 1
     return count
-    
+
 
 def ans2():
     count = 0
-    slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)]
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     ans = 1
     for (dc, dr) in slopes:
         c = 0
@@ -21,14 +21,14 @@ def ans2():
         while r < len(G):
             c += dc
             r += dr
-            if r < len(G) and G[r][c%len(G[r])] == '#':
+            if r < len(G) and G[r][c % len(G[r])] == "#":
                 count += 1
         ans *= count
     return ans
-        
+
 
 G = []
-with open('input') as f:
+with open("input") as f:
     for line in f:
         G.append(list(line.strip()))
 
