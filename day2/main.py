2493d988c99d9ab5e02411f1da301a7470726b74
sorachii <bencecube@gmail.com>
2020-12-06 19:51:10 +0100

    reformatted day2/main.py with black

--- day2/main.py
+++ day2/main.py
@@ -1,16 +1,18 @@
 import re
 
-pattern = re.compile(r'(\d+)-(\d+) (\w): (\w*)', re.A)
+pattern = re.compile(r"(\d+)-(\d+) (\w): (\w*)", re.A)
 
 lista = []
 
-with open('input') as f:
+with open("input") as f:
     for line in f:
         lista.append(line)
+
 
 def parse(line):
     lo, hi, char, string = re.match(pattern, line).groups()
     return int(lo), int(hi), char, string
+
 
 def ans1():
     count = 0
@@ -20,6 +22,7 @@ def ans1():
             count += 1
     return count
 
+
 def ans2():
     count = 0
     for line in lista:
@@ -27,6 +30,7 @@ def ans2():
         if (string[lo - 1] == char) != (string[hi - 1] == char):
             count += 1
     return count
+
 
 # just for testing I wanted to print a 'correct' line
 def ans3():
