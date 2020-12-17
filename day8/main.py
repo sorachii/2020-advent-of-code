ffac5d0002c7dcb1a076b8e46f569510752e389a
sorachii <bencecube@gmail.com>
2020-12-17 23:33:01 +0100

    human coding, I like that

--- day8/main.py
+++ day8/main.py
@@ -1,25 +1,40 @@
 lines = []
-with open("input") as f:
+with open("input.sample") as f:
     for line in f:
         lines.append(line.split())
 
 
 def ans1(operations):
-    acc = 0
-    i = 0
+    acc, i = 0, 0
     steps = set()
-    while i < len(operations) and i not in steps:
+    while True:
+        if i in steps:
+            return (False, acc)
+        elif i == len(lines):
+            return(True, acc)
+        steps.add(i)
         if operations[i][0] == "nop":
-            steps.add(i)
             i += 1
         elif operations[i][0] == "acc":
-            steps.add(i)
             acc += int(operations[i][1])
             i += 1
         elif operations[i][0] == "jmp":
-            steps.add(i)
             i += int(operations[i][1])
-    return acc
+
+
+def ans2(operations):
+    to_change = (i for i, inst in enumerate(operations) if inst[0] in ('nop', 'jmp'))
+    acc, i = 0, 0
+    steps = set()
+    for i in to_change:
+        new_instructions = list(operations)
+        print(new_instructions[i][0])
+        new_instructions[i][0] = "jmp" if operations[i][0] == 'nop' else 'nop'
+        halts, acc = ans1(new_instructions)
+        if halts:
+            break
+    return(acc)
 
 
 print(ans1(lines))
+print(ans2(lines))
