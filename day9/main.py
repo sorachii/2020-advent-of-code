334516e091dbcb3be6b54cb645bcd64fb8ab1a2b
sorachii <bencecube@gmail.com>
2020-12-20 00:20:33 +0100

    day9 star2

--- day9/main.py
+++ day9/main.py
@@ -5,11 +5,21 @@ with open("input") as f:
 
 def ans1(nums):
     for i, n in enumerate(nums):
-        if i < 25:                  # 25 == preamble
+        if i < 25:                  # preamble
             continue
         prev = nums[i-25:i]
         if not any(n == n1 + n2 for n1 in prev for n2 in prev):
             return(n)
 
 
-print(ans1(nums))
+def ans2(nums):
+    for size in range(2, len(nums)):
+        for i in range(len(nums) - size):
+            s = nums[i:i+size]
+            if sum(s) == invalid:
+                return(min(s) + max(s))
+
+
+invalid = ans1(nums)
+print(invalid)
+print(ans2(nums))
