7aba567edd1a9f605b634ddd0fa4dfa36f11cf24
sorachii <bencecube@gmail.com>
2020-12-20 01:32:26 +0100

    day10/2 mood:angry

--- day10/main.py
+++ day10/main.py
@@ -1,4 +1,4 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
 
 with open("input") as f:
     jrs = [int(jr) for jr in f.readlines()]
@@ -18,4 +18,16 @@ def ans1(jrs):
             return diffs.count(1) * diffs.count(3)
 
 
+def ans2(jrs):
+    paths = defaultdict(int)
+    paths[0] = 1
+    for i in range(1, len(jrs)):
+        for j in range(i)[::-1]:
+            if jrs[i] - jrs[j] > 3:
+                break
+            paths[i] += paths[j]
+    return(paths[len(jrs)-1])
+
+
 print(ans1(jrs))
+print(ans2(jrs))
