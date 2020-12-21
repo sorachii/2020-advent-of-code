9a19696f318c6d35fe94250176138ad58f1bdff2
sorachii <bencecube@gmail.com>
2020-12-22 00:37:59 +0100

    day12/2 interesting challenge

--- day12/main.py
+++ day12/main.py
@@ -3,11 +3,11 @@ with open("input") as f:
 
 
 data = [(line[0], int(line[1:])) for line in lines]
+dirs = {'N': 1j, 'E': 1, 'S': -1j, 'W': -1}
+rot = {'L': 1j, 'R': -1j}
 
 
 def ans1(data):
-    dirs = {'N': 1j, 'E': 1, 'S': -1j, 'W': -1}
-    rot = {'L': 1j, 'R': -1j}
     loc = 0
     d = 1
     for act, value in data:
@@ -17,8 +17,21 @@ def ans1(data):
             d *= rot[act] ** (value/90)
         else:
             loc += d * value
-
     return(abs(loc.real) + abs(loc.imag))
 
 
+def ans2(data):
+    loc = 0
+    waypoint = 10 + 1j
+    for act, value in data:
+        if act in dirs:
+            waypoint += dirs[act] * value
+        elif act in rot:
+            waypoint *= rot[act] ** (value/90)
+        else:
+            loc += waypoint * value
+    return (abs(loc.real) + abs(loc.imag))
+
+
 print(ans1(data))
+print(ans2(data))
