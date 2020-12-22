dd1b40af71be6e04b2216af5f94f418e19ff2550
sorachii <bencecube@gmail.com>
2020-12-22 01:45:43 +0100

    day13/2 exploring libs(sympy)

--- day13/main.py
+++ day13/main.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3.7
+from sympy.ntheory.modular import solve_congruence
+
+
 with open("input") as f:
     lines = list(f.readlines())
+
+
+bus_times = [(-i, int(x)) for i, x in enumerate(lines[1].split(',')) if x != 'x']
 
 
 def ans1(lines):
@@ -9,4 +16,9 @@ def ans1(lines):
     return((-n) % x * x)
 
 
+def ans2(bus_times):
+    return(solve_congruence(*bus_times)[0])
+
+
 print(ans1(lines))
+print(ans2(bus_times))
