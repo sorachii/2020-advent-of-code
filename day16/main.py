457553185fc416865e84ca9bd1e139439f1838cd
sorachii <bencecube@gmail.com>
2020-12-27 23:09:26 +0100

    day16/2: I hate when I get good answer for part1 and then spend a ton of time debugging part2 only to figure out after wasting a ton of time that my part 1 parsing was wrong. The parser outsmarted me.

--- day16/main.py
+++ day16/main.py
@@ -1,11 +1,13 @@
 import re
 
 import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import maximum_bipartite_matching
 
 with open("input") as f:
     ls = [line.strip() for line in f.readlines()]
 
-ranges = [list(map(int, re.findall('\d+', x))) for x in ls[:19]]
+ranges = [list(map(int, re.findall('\d+', x))) for x in ls[:20]]
 your = np.array([int(x) for x in ls[22].split(',')], dtype=np.int64)
 nearby = [list(map(int, re.findall('\d+', x))) for x in ls[25:]]
 
@@ -16,3 +18,11 @@ for t1, t2, t3, t4 in ranges:
 
 # Answer 1
 print(sum(n for l in nearby for n in l if n not in valid))
+
+# Answer 2
+valids = [l for l in nearby if all(n in valid for n in l)]
+loc = [[all((t1 <= l[j] <= t2) or (t3 <= l[j] <= t4) for l in valids)
+        for t1, t2, t3, t4 in ranges]
+       for j in range(20)]
+m = maximum_bipartite_matching(csr_matrix(loc))
+print(your[m[:6]].prod())
