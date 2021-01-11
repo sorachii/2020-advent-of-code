a294d3aa94dc495daee5842e89dba9abf4d00eab
sorachii <bencecube@gmail.com>
2021-01-11 23:46:38 +0100

    day19/2: I feel like the rest won't be easier

--- day19/main.py
+++ day19/main.py
@@ -4,8 +4,10 @@ from lark import Lark
 with open('input') as f:
     data = f.read()
 
+
 rules, messages = data.split('\n\n')
 messages = messages.split()
+
 
 def solve(rules):
     rules = 'start: t0\n' + re.sub(r'(\d+)', r't\1', rules)
@@ -18,4 +20,9 @@ def solve(rules):
             valid -= 1
     return valid
 
+
+print(solve(rules))
+rules = rules\
+        .replace('8: 42', '8: 42 | 42 8')\
+        .replace('11: 42 31', '11: 42 31 | 42 11 31')
 print(solve(rules))
